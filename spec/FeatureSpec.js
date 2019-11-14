e76479c8b5a16291967c21b80170f5fcbe759492
Lena Krasnova <olkras03@gmail.com>
2019-11-14 18:41:56 +0000

    added unit test for a plane

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,23 @@
+// First user story
+
+//As an air traffic controller
+// To get passengers to a destination
+// I want to instruct a plane to land at
+//   an airport and confirm that it has landed
+
+"use strict";
+
+describe("Feature Test:", function() {
+  var plane;
+  var airport;
+
+  beforeEach(function() {
+    plane = new plane();
+    airport = new airport();
+  });
+
+  it("planes can be instructed to land at an airport", function() {
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+});
