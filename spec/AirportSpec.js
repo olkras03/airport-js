21876a66ff3d5364f0ba42eec86e9faf00928fd7
Lena Krasnova <olkras03@gmail.com>
2019-11-15 16:04:45 +0000

    some refactoring, but still two tests failing

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,15 +3,23 @@
 describe("Airport", function() {
   var airport;
   var plane;
+  var weather
+
   beforeEach(function() {
-    airport = new Airport();
-    plane = jasmine.createSpy("plane", ["land"]);
+    plane = jasmine.createSpy('plane');
+    weather = jasmine.createSpy('weather, ['isStormy']);
+    airport = new Airport(weather);
+    // plane = jasmine.createSpy("plane", ["land"]);
   });
 
   it("has no planes by default", function() {
     expect(airport.planes()).toEqual([]);
   });
 
+  describe('under normal conditions', function() {
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(false);
+    });
   it("can clear planes for landing", function() {
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
@@ -22,12 +30,16 @@ describe("Airport", function() {
     airport.clearForTakeOff(plane);
     expect(airport.planes()).toEqual([]);
   });
-
+   
+})
   it("can check for stromy conditions", function() {
     expect(airport.isStormy()).toBeFalsy();
   });
 
   describe("under stormy conditions", function() {
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(true);
+    });
     it("does not clear the planes for takeoff", function() {
       spyOn(airport, "isStormy").and.returnValue(true);
       expect(function() {
--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -15,21 +15,25 @@ describe("Feature Test:", function() {
     plane = new Plane();
     airport = new Airport();
   });
+  describe("under normal conditions", function() {
+    beforeEach(function() {
+      spyOn(Math, "random").and.returnValue(0);
+    });
+    it("planes can be instructed to land at an airport", function() {
+      plane.land(airport);
+      expect(airport.planes()).toContain(plane);
+    });
 
-  it("planes can be instructed to land at an airport", function() {
-    plane.land(airport);
-    expect(airport.planes()).toContain(plane);
-  });
+    // As an air traffic controller
+    // To get passengers to a destination
+    // I want to instruct a plane to take off from
+    //   an airport and confirm that it is no longer in the airport
 
-  // As an air traffic controller
-  // To get passengers to a destination
-  // I want to instruct a plane to take off from
-  //   an airport and confirm that it is no longer in the airport
-
-  it("planes can be instructed to takeoff", function() {
-    plane.land(airport);
-    plane.takeoff();
-    expect(airport.planes()).not.toContain(plane);
+    it("planes can be instructed to takeoff", function() {
+      plane.land(airport);
+      plane.takeoff();
+      expect(airport.planes()).not.toContain(plane);
+    });
   });
 
   describe("under stormy conditions", function() {
@@ -38,8 +42,9 @@ describe("Feature Test:", function() {
     // I want to prevent takeoff when weather is stormy
 
     it("stops takeoff when weather is stormy", function() {
+      spyOn(Math, "random").and.returnValue(0);
       plane.land(airport);
-      spyOn(airport, "isStormy").and.returnValue(true);
+      spyOn(airport._weather,'isStormy').and.returnValue(true);
       expect(function() {
         plane.takeoff();
       }).toThrowError("cannot takeoff during storm");
@@ -51,8 +56,7 @@ describe("Feature Test:", function() {
     // I want to prevent landing when weather is stormy
 
     it("prevents landing when weather is stormy", function() {
-      plane.land(airport);
-      spyOn(airport, "isStormy").and.returnValue(true);
+      spyOn(Math, "random").and.returnValue(1);
       expect(function() {
         plane.land(airport);
       }).toThrowError("cannot land during storm");
