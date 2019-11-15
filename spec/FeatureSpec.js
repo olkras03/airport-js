85a7480ea7577917934a38b9643ac1f8d11ae41e
Lena Krasnova <olkras03@gmail.com>
2019-11-15 12:54:57 +0000

    Implementing second user story
     As an air traffic controller
     To get passengers to a destination
     I want to instruct a plane to take off from
      an airport and confirm that it is no longer in the airport
    
     Failing feature test for planes to be instructed to take off.

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -20,4 +20,19 @@ describe("Feature Test:", function() {
     plane.land(airport);
     expect(airport.planes()).toContain(plane);
   });
+
+// As an air traffic controller
+// To get passengers to a destination
+// I want to instruct a plane to take off from
+//   an airport and confirm that it is no longer in the airport
+
+  it('planes can be instructed to takeoff', function(){
+    plane.land(airport)
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+  
 });
+
+
+
