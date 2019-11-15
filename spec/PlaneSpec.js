0e14da5ce31c13c28a487ffe62006ae92853b031
Lena Krasnova <olkras03@gmail.com>
2019-11-15 13:00:05 +0000

    matching failing unit test for plane to be able to take off from the airport

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -7,8 +7,15 @@ describe('Plane',function(){
     plane = new Plane();
     airport = jasmine.createSpyObj('airport',['clearForLanding']);
   });
+
   it('can land at an airport', function(){
     plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+  });
+  
+  it('can takeoff from an airport', function(){
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.clearForTakeOff).toHaveBeenCalled();
   });
 });
